30bbed23f06bf72cc56ef8c7acf92612157071e6
vikepk <prempk0516@gmail.com>
2023-03-30 17:59:24 +0530

    Changes Made

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
@@ -9,6 +10,17 @@ class ChooseLocation extends StatefulWidget {
 
 class _ChooseLocationState extends State<ChooseLocation> {
   int count = 0;
+
+  List<Worldtime> locations = [
+    Worldtime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    Worldtime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    Worldtime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    Worldtime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    Worldtime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    Worldtime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    Worldtime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    Worldtime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
 
   @override
   Widget build(BuildContext context) {
