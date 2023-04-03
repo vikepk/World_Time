1c19270e7984615117a15b1b048ae1fc96f43dfc
vikepk <prempk0516@gmail.com>
2023-04-03 12:08:43 +0530

    Final

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -11,7 +11,7 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
   List<Worldtime> locations = [
     Worldtime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
-    Worldtime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    Worldtime(url: 'Europe/Berlin', location: 'Berlin', flag: 'greece.png'),
     Worldtime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
     Worldtime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
     Worldtime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
